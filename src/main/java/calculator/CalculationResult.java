d99de07c70fd326c0a89ec12b81e0b935f0c5344
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-11-26 14:57:49 +0000

    Added gradle

--- src/main/java/calculator/CalculationResult.java
+++ src/main/java/calculator/CalculationResult.java
@@ -25,4 +25,9 @@ public class CalculationResult {
     public int hashCode() {
         return Objects.hash(value);
     }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
 }
